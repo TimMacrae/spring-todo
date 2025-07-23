c41e3a221c622b2ca0b40550eb936eb641d1385d
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-23 12:52:33 +0200

    remove unused code

--- src/main/java/com/ecosystem/todojava/repository/TodoRepository.java
+++ src/main/java/com/ecosystem/todojava/repository/TodoRepository.java
@@ -4,9 +4,7 @@ import com.ecosystem.todojava.model.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface TodoRepository extends MongoRepository<Todo, String> {
-    List<Todo> findAllByStatus(String status);
 }
