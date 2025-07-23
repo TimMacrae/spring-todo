99c2162f2df0a10af2f1bd928eefe709d3b2e591
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-23 11:03:50 +0200

    add methods get/post service

--- src/main/java/com/ecosystem/todojava/service/TodoService.java
+++ src/main/java/com/ecosystem/todojava/service/TodoService.java
@@ -1,11 +1,31 @@
 package com.ecosystem.todojava.service;
 
+import com.ecosystem.todojava.exception.TodoCouldNotBeCreated;
+import com.ecosystem.todojava.model.Todo;
+import com.ecosystem.todojava.model.TodoDto;
 import com.ecosystem.todojava.repository.TodoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class TodoService {
     private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo createTodo(TodoDto todoDto) {
+        try {
+            Todo todo = new Todo(UUID.randomUUID().toString(),todoDto.description(),todoDto.status(), LocalDateTime.now(),null);
+            return todoRepository.save(todo);
+        }catch (Exception e){
+            throw new TodoCouldNotBeCreated(todoDto.description());
+        }
+    }
 }
